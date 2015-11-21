4ecf752ae66ff4513e0214b7ea2366c567b1a1b2
Andrew Dorokhov <andrew@dorohoff.net>
2015-11-22 01:36:29 +0200

    Add main window frame.

--- PortTester.java
+++ PortTester.java
@@ -1,9 +1,23 @@
+import javax.swing.JFrame;
+
 /*
  * PortTester - GUI network tool for testing TCP-ports
  * @author Andrew Dorokhov
  */
 public class PortTester {
 	public static void main(String[] args) {
-		
+		PortTesterFrame frame = new PortTesterFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
+}
+
+class PortTesterFrame extends JFrame {
+	
+	public PortTesterFrame() {
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	}
+	
+	public static final int DEFAULT_WIDTH = 400;
+	public static final int DEFAULT_HEIGHT = 400;
 }
