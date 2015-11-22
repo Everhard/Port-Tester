a585fee981cad2cd3de0ee510b7a6a483a873048
Andrew Dorokhov <andrew@dorohoff.net>
2015-11-22 14:07:34 +0200

    Add header component.

--- PortTester.java
+++ PortTester.java
@@ -1,4 +1,5 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 /*
  * PortTester - GUI network tool for testing TCP-ports
@@ -6,21 +7,48 @@ import javax.swing.JFrame;
  */
 public class PortTester {
 	public static void main(String[] args) {
-		PortTesterFrame frame = new PortTesterFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Port Tester");
-		frame.setVisible(true);
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				PortTesterFrame frame = new PortTesterFrame();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		});
 	}
 }
 
 class PortTesterFrame extends JFrame {
 	
 	public PortTesterFrame() {
+		setTitle("Port Tester");
 		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 		setResizable(false);
 		setLocationByPlatform(true);
+		
+		/*
+		 * Add header component:
+		 */
+		HeaderComponent header = new HeaderComponent();
+		add(header);
 	}
 	
 	public static final int DEFAULT_WIDTH = 400;
 	public static final int DEFAULT_HEIGHT = 400;
+}
+
+class HeaderComponent extends JComponent {
+	public void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		
+		/*
+		 * Set font:
+		 */
+		Font headerFont = new Font("Helvetica", Font.PLAIN, 24);
+		g2.setFont(headerFont);
+		
+		g2.drawString("Port Tester", MESSAGE_X, MESSAGE_Y);
+	}
+	
+	public static final int MESSAGE_X = 100;
+	public static final int MESSAGE_Y = 50;
 }
